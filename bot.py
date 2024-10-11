386c0bce2a01e078b77eb7e4d5e0eeb1cef75c7c
Bruna Ferreira <bferreira.slv@gmail.com>
2024-10-11 12:07:10 -0300

    processo de login e logout

--- bot.py
+++ bot.py
@@ -1,82 +1,79 @@
-"""
-WARNING:
-
-Please make sure you install the bot dependencies with `pip install --upgrade -r requirements.txt`
-in order to get all the dependencies on your Python environment.
-
-Also, if you are using PyCharm or another IDE, make sure that you use the SAME Python interpreter
-as your IDE.
-
-If you get an error like:
-```
-ModuleNotFoundError: No module named 'botcity'
-```
-
-This means that you are likely using a different Python interpreter than the one used to install the dependencies.
-To fix this, you can either:
-- Use the same interpreter as your IDE and install your bot with `pip install --upgrade -r requirements.txt`
-- Use the same interpreter as the one used to install the bot (`pip install --upgrade -r requirements.txt`)
-
-Please refer to the documentation for more information at
-https://documentation.botcity.dev/tutorials/python-automations/web/
-"""
-
-
-# Import for the Web Bot
-from botcity.web import WebBot, Browser, By
-
-# Import for integration with BotCity Maestro SDK
 from botcity.maestro import *
+from botcity.web import Browser, By, WebBot
+from webdriver_manager.firefox import GeckoDriverManager
 
-# Disable errors if we are not connected to Maestro
+# Flag para executar localmente sem autenticação com Orquestrador
 BotMaestroSDK.RAISE_NOT_CONNECTED = False
 
 
 def main():
-    # Runner passes the server url, the id of the task being executed,
-    # the access token and the parameters that this task receives (when applicable).
+    # Autenticação feita via Runner
     maestro = BotMaestroSDK.from_sys_args()
-    ## Fetch the BotExecution with details from the task, including parameters
+    # Retorna informações de execução
     execution = maestro.get_execution()
-
-    print(f"Task ID is: {execution.task_id}")
-    print(f"Task Parameters are: {execution.parameters}")
 
     bot = WebBot()
 
-    # Configure whether or not to run on headless mode
+    # Configurações do navegador
     bot.headless = False
+    bot.browser = Browser.FIREFOX
+    bot.driver_path = GeckoDriverManager().install()
 
-    # Uncomment to change the default Browser to Firefox
-    # bot.browser = Browser.FIREFOX
+    # Demo de login na plataforma
+    try:
+        bot.browse("https://practicetestautomation.com/practice-test-login/")
 
-    # Uncomment to set the WebDriver path
-    # bot.driver_path = "<path to your WebDriver binary>"
+        # login
+        user = bot.find_element(
+            selector="#login > ul:nth-child(2) > li:nth-child(2) > b:nth-child(2)",
+            by=By.CSS_SELECTOR)
+        bot.find_element("username", By.ID).send_keys(user.text)
 
-    # Opens the BotCity website.
-    bot.browse("https://www.botcity.dev")
+        passw = bot.find_element(
+            selector="#login > ul:nth-child(2) > li:nth-child(2) > b:nth-child(4)",
+            by=By.CSS_SELECTOR)
+        bot.find_element("password", By.ID).send_keys(passw.text)
 
-    # Implement here your logic...
-    ...
+        bot.find_element("submit", By.ID).click()
 
-    # Wait 3 seconds before closing
-    bot.wait(3000)
+        # salve captura de tela e envia para o Orquestrador
+        bot.wait(3000)
+        bot.save_screenshot(r"resources\sucesso.png")
+        maestro.post_artifact(
+            task_id=execution.task_id,
+            artifact_name="sucesso.png",
+            filepath=r"resources\sucesso.png"
+        )
 
-    # Finish and clean up the Web Browser
-    # You MUST invoke the stop_browser to avoid
-    # leaving instances of the webdriver open
-    bot.stop_browser()
+        # desloga
+        bot.find_element("//a[text()='Log out']", By.XPATH).click()
 
-    # Uncomment to mark this task as finished on BotMaestro
-    # maestro.finish_task(
-    #     task_id=execution.task_id,
-    #     status=AutomationTaskFinishStatus.SUCCESS,
-    #     message="Task Finished OK."
-    # )
+        # status de finalização
+        status = AutomationTaskFinishStatus.SUCCESS
+        message = "Processo OK."
 
+    except Exception as error:
+        bot.save_screenshot(r"resources\falha.png")
+        maestro.error(
+            task_id=execution.task_id,
+            exception=error,
+            screenshot=r"resources\falha.png"
+        )
 
-def not_found(label):
-    print(f"Element not found: {label}")
+        # status de finalização
+        status = AutomationTaskFinishStatus.FAILED
+        message = "Processo FALHOU."
+
+    finally:
+        bot.wait(3000)
+        bot.stop_browser()
+
+        # Finaliza tarefa no Orquestrador
+        maestro.finish_task(
+            task_id=execution.task_id,
+            status=status,
+            message=message
+        )
 
 
 if __name__ == '__main__':
